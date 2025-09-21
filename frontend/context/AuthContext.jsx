d08d24f6dd964afed830d34ad24c764cfd0572e2
Ahmed <ahmedkhaled.dev02@gmail.com>
2025-09-21 07:01:21 +0300

    Add full project code

---
+++ frontend/context/AuthContext.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react-refresh/only-export-components */
+
+import { createContext, useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decode = jwtDecode(token);
+      setUser(decode);
+    }
+  }, []);
+
+  const login = (token) => {
+    localStorage.setItem("token", token);
+    const decode = jwtDecode(token);
+    setUser(decode);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
